f7b49e1e9d92a4c27f4165e32815538c37add38f
Choi Jun Sung <junsung.choi@streami.co>
2021-09-11 18:45:22 +0900

    Add doobooui font import code for Icon component

--- docs/src/components/IconButton/index.tsx
+++ docs/src/components/IconButton/index.tsx
@@ -1,16 +1,30 @@
-import {ThemeProvider, Icon, IconButton} from 'dooboo-ui';
+import {ThemeProvider, IconButton, Icon} from 'dooboo-ui';
 import React, {ReactElement} from 'react';
 
 import {View} from 'react-native';
 import styled from '@emotion/native';
+import {useFonts} from 'expo-font';
 
 const StyledIcon = styled(Icon)`
   color: ${({theme}) => theme.textContrast};
 `;
 
 const IconButtonStory = (): React.ReactElement => {
+  const [fontsLoaded] = useFonts({
+    IcoMoon: require('../../../../main/Icon/doobooui.ttf'),
+  });
+
+  if (!fontsLoaded) return <View />;
+
   return (
-    <View>
+    <View
+      style={{
+        width: '100%',
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <IconButton icon={<StyledIcon size={24} name="moment-solid" />} />
       <View style={{width: 8}} />
       <IconButton icon={<StyledIcon size={24} name="add-light" />} />
